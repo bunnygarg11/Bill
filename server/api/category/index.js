5dd7b0835badf742f30dc8e921b889b30700074b
mohitgarg <mohitgarg083@gmail.com>
2020-01-22 18:58:48 +0530

    22th

--- server/api/category/index.js
+++ server/api/category/index.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const userAuth = require("../../../server/middlewares/userAuth")
 
 const { categoryList } = require("./controllers/getCategory")
+const { selectCategory} = require("./controllers/selectCategory")
 
 router.get("/categoryList", userAuth, categoryList)
+router.post("/selectcategory", userAuth, selectCategory)
 module.exports = router;
--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const userAuth=require("./../../middlewares/userAuth")
+const {check,validationResult}=require("express-validator")
 
 const { signup } = require("./controllers/signup")
 const { login } = require('./controllers/login');
@@ -14,7 +15,37 @@ const { verifyEmail } = require("./controllers/verifyEmail")
 
 router.get("/verifyemail/:token", verifyEmail)
 router.get("/verify/:token", verify)
-router.post("/register", signup)
+router.post("/register", [
+    // check("name","Name is required").not().isEmpty(),
+    // check("email","please include valid email").isEmail(),
+    // check("password","Please enter a password with 6 or more characters").isLength({  min:6})
+    check("shopDetails.*.legalName","legal Name is required").custom(((value,{req})=>{
+        // req.body.shopDetails.isGstNumber==="yes"?value!="":true
+        if(req.body.shopDetails.isGstNumber==="yes"||req.body.shopDetails.isGstNumber===1){
+            req.body.shopDetails.isGstNumber=1
+            return value!=""
+        }
+        return true
+    })),
+    check("shopDetails.*.gstNumber","Gst Number is required").custom(((value,{req})=>{
+        // req.body.shopDetails.isGstNumber==="yes"?value!="":true
+        if(req.body.shopDetails.isGstNumber==="yes"||req.body.shopDetails.isGstNumber===1){
+            req.body.shopDetails.isGstNumber=1
+            return value!=""
+        }
+        return true
+    })),
+    check("paymentDetails.*.onlineStore","Online store details required").custom(((value,{req})=>{
+        req.body.paymentDetails.isSellOnline==="yes"?value!=="" && value.length!=0:true
+    })),
+    check("shopDetails.*.countryId","Country is required").not().isEmpty(),
+    check("shopDetails.*.stateId","state is required").not().isEmpty(),
+    check("shopDetails.*.cityId","city is required").not().isEmpty(),
+    check("socialId","socialid is required").custom(((value,{req})=>{
+     
+      req.body.registerType==="Custom"?true:value!=""
+    })),
+],signup)
 // router.post("/resetPassword", resetPassword)
 router.post("/changePassword",userAuth, changePassword)
 router.post("/login", login)
